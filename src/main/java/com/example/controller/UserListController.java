1922fd67efc17e69361ecbc558ae503096542511
asad194 <asdddd194@gmail.com>
2021-08-17 23:57:47 +0900

    位置No3575まで。UserListController改修。

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -1,15 +1,36 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController {
- /** ユーザー一覧画面を表示 */
+
+	@Autowired
+	private UserService userService;
+
+	 /** ユーザー一覧画面を表示。
+	  *
+	  * ユーザーサービスからユーザーの一覧を取得して、その結果をModelに登録しています。
+	  *  @author asad
+	  */
 	@GetMapping("/list")
-	public String getUserList() {
+	public String getUserList(Model model) {
+		// ユーザー一覧取得
+		List<MUser> userList = userService.getUsers();
+
+		// Modelに登録
+		model.addAttribute("userList", userList);
+
 		//ユーザー一覧画面を表示
 		return "user/list";
 	}
