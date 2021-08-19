4d4867f4d830edfa717939afc6c84dc8259329ed
asad194 <asdddd194@gmail.com>
2021-08-19 17:54:51 +0900

    IntelliJで初のコミット&PUSHしてみた

--- src/main/java/com/example/SpringBootSampleApplication.java
+++ src/main/java/com/example/SpringBootSampleApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSampleApplication {
-
+	
+	/**
+	 * メインメソッド
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
